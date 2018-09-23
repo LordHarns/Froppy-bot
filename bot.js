111bbf64733ec186e899475d8943c3c00129a092
LordHarns <hlgoetz@sbcglobal.net>
2018-09-22 20:08:51 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -343,7 +343,10 @@ bot.on("message", async message =>{
         message.channel.send("https://imgur.com/a/BA125vw");
         return;
     }
-    
+     if(command ===`${prefix}mineta`){
+        message.channel.send("A short, weak, and useless character whos only personality trait that stops him from being entirely forgotten is the fact that he is a pervert, and only exist to sell plushy toys does not deserve a command.");
+        return;
+    }
   
 
 
