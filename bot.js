4d60233f18e9a4b7c9676d949a32025fab80802b
LordHarns <hlgoetz@sbcglobal.net>
2018-11-13 20:41:48 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -428,7 +428,7 @@ bot.on("message", async message =>{
         return;
     }
     if(command ===`${prefix}creepyuncle`){
-        message.channel.send("<@237622107138293760>. you have been summoned.");
+        message.channel.send("<@237622107138293760>. I choose you!");
         return;
     }
     
